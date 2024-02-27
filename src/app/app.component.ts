01e57afbb2715709e0109546cc994988cfc2e2c4
junaid aslam <junaidaslam322@gmail.com>
2024-02-27 06:42:57 -0800

    reactive form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ReactiveFormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'angular-form';
+  buildForm = inject(FormBuilder);
+  user: FormGroup = this.buildForm.group({
+    selectedCountry: [''],
+    city: [''],
+  });
+  onSubmit() {
+    var formValue = this.user.value;
+    console.log('Your form data : ', formValue);
+    alert('data added successfully');
+  }
 }
